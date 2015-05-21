0fddf842f73afb391c212003395193383b35975e
liamrahav <liorogamerdvd@gmail.com>
2015-05-21 16:10:27 -0400

    Set up repo's gruntfile + npm modules

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+		grunt.initConfig({
+			sass: {
+				compile: {
+					options: {
+						style: 'compressed',
+						sourcemap: 'none'
+					},
+					files: {
+						'css/styles.css': 'css/sass/styles.scss'
+					}
+				}
+			},
+			
+			watch: {
+				style: {
+					files: "css/**/*.scss",
+					tasks: ['sass']
+				}
+			}
+		});
+		
+		grunt.loadNpmTasks('grunt-contrib-watch');
+		grunt.loadNpmTasks('grunt-contrib-sass');
+		
+		grunt.registerTask('default', ['sass:compile', "watch"]);
+}
